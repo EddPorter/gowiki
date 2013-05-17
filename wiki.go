4c6d537d0e5d273464570e477f3f2294ec15f177
Edd Porter <git@eddporter.com>
2013-05-17 21:13:50 +0100

    Create simple web server

--- wiki.go
+++ wiki.go
@@ -3,12 +3,15 @@ package main
 import (
   "fmt"
   "io/ioutil"
+  "net/http"
 )
 
 type Page struct {
   Title string
   Body  []byte	// a byte "slice". we don't use 'string' as '[]byte' is expected by io libraries
 }
+
+const lenPath = len("/view/")
 
 // This is a method named 'save' that takes as its receiver p, a pointer to Page.
 // It take no parameters and returns a value of type 'error'.
@@ -27,9 +30,20 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[lenPath:]
+  p, _ := loadPage(title)
+  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+//func main() {
+//  p1 := &Page{Title: "TestPage", Body: []byte("This is a simple Page.")}
+//  p1.save()
+//  p2, _ := loadPage("TestPage")
+//  fmt.Println(string(p2.Body))
+//}
+
 func main() {
-  p1 := &Page{Title: "TestPage", Body: []byte("This is a simple Page.")}
-  p1.save()
-  p2, _ := loadPage("TestPage")
-  fmt.Println(string(p2.Body))
+  http.HandleFunc("/view/", viewHandler)
+  http.ListenAndServe(":8080", nil)
 }
