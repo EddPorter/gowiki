4ec2e9dae6a04acfa7534ae5cda3873dc9e1cf28
Edd Porter <git@eddporter.com>
2013-05-17 22:40:52 +0100

    Initialise templates once

--- wiki.go
+++ wiki.go
@@ -13,6 +13,10 @@ type Page struct {
 
 const lenPath = len("/view/")
 
+// The function template.Must is a convenience wrapper that panics when
+// passed a non-nil error value, and otherwise returns the *Template unaltered
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 // This is a method named 'save' that takes as its receiver p, a pointer to Page.
 // It take no parameters and returns a value of type 'error'.
 func (p *Page) save() error {
@@ -31,12 +35,7 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-  t, err := template.ParseFiles(tmpl + ".html")
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  err = t.Execute(w, p)
+  err := templates.ExecuteTemplate(w, tmpl+".html", p)
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
